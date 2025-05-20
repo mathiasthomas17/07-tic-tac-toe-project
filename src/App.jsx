b0d18b56452a5f81e4b77704bb9767a9b653905f
mathiasthomas17 <thomasmathias17@gmail.com>
2025-05-20 06:47:09 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Player from "./components/Player"
-
+import { useState } from "react"
 
 function App() {
   
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Player = ({name,symbol}) => {
+
+    const [editing,setIsEditing] = useState(false)
+    const handleEditing = function(){
+        setIsEditing(!editing)
+    }
+  
   return (
-    <li>
+    <>
+    { editing ? (
+        <input type='text' required/>
+    ):(
+        <li>
               <span className="player">
               <span className="player-name">{name}</span>
               <span className="player-symbol">{symbol}</span>
               </span>
-              <button>Edit</button>
+              <button onClick={handleEditing}>Edit</button>
     </li>
+    )}
+    </>
   )
 }
 
 export default Player
+
+// Repeat Lec 77
